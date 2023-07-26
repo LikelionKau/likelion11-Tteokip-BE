3e16310506114cf70d2c8098f5df54ffa5110d22
seosang-won <zmdk1205@naver.com>
2023-07-26 11:49:15 +0900

    Initial Settings

---
+++ src/main/java/com/example/koun/KounApplication.java
@@ -0,0 +1,13 @@
+package com.example.koun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KounApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KounApplication.class, args);
+    }
+
+}
