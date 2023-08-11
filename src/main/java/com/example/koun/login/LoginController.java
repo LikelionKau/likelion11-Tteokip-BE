93b878dc354e38e8812f1f32329f2a32dd186ef1
seosang-won <zmdk1205@naver.com>
2023-08-11 14:06:02 +0900

    LKLN-41 Feat: login 인가처리 내용: 로그인 인가처리 커밋 , 다른 feature에 로그인 작업내용이 반영되어있었음.?

--- src/main/java/com/example/koun/login/LoginController.java
+++ src/main/java/com/example/koun/login/LoginController.java
@@ -38,18 +38,9 @@ public class LoginController {
 
 
 
-
-
-    @GetMapping("/")
-    public String login(){
-        return "loginTest";
-    }
-
-
-
-    @RequestMapping(value="/loginRedirect", method=RequestMethod.GET)
-    public String kakaoLogin(@RequestParam(value = "code", required = false) String code
-    , HttpServletResponse httpResponse) throws Exception {
+    @RequestMapping(value = "/loginRedirect", method = RequestMethod.GET)
+    public ResponseEntity<String> kakaoLogin(@RequestParam(value = "code", required = false) String code
+            , HttpServletResponse httpResponse) throws Exception {
         System.out.println("카카오로부터 요청받은 인가코드 : " + code);
 
 
@@ -65,13 +56,13 @@ public class LoginController {
         params.add("code", code);
 
         HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params,
-            headers);
+                headers);
 
         ResponseEntity<String> response = rt.exchange(
-            "https://kauth.kakao.com/oauth/token", // https://{요청할 서버 주소}
-            HttpMethod.POST, // 요청할 방식
-            kakaoTokenRequest, // 요청할 때 보낼 데이터
-            String.class // 요청 시 반환되는 데이터 타입
+                "https://kauth.kakao.com/oauth/token", // https://{요청할 서버 주소}
+                HttpMethod.POST, // 요청할 방식
+                kakaoTokenRequest, // 요청할 때 보낼 데이터
+                String.class // 요청 시 반환되는 데이터 타입
         );
 
         System.out.println(" 카카오 토큰 요청 완료: 토큰 요청에 대한 응답 :" + response);
@@ -79,15 +70,15 @@ public class LoginController {
         ObjectMapper objectMapper = new ObjectMapper();
         OAuthToken oauthToken = null;
 
-        try{
-             oauthToken = objectMapper.readValue(response.getBody(), OAuthToken.class);
-        }catch(JsonMappingException e){
+        try {
+            oauthToken = objectMapper.readValue(response.getBody(), OAuthToken.class);
+        } catch (JsonMappingException e) {
             e.printStackTrace();
-        }catch (JsonProcessingException e){
+        } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
 
-        System.out.println("카카오 액세스 토큰 : "+oauthToken.getAccess_token());
+        System.out.println("카카오 액세스 토큰 : " + oauthToken.getAccess_token());
 
         RestTemplate rt2 = new RestTemplate();
 
@@ -96,63 +87,53 @@ public class LoginController {
         headers2.add("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
 
 
-
         HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest2 = new HttpEntity<>(params,
-            headers2);
+                headers2);
 
         ResponseEntity<String> response2 = rt2.exchange(
 
-            "https://kapi.kakao.com/v2/user/me", // https://{요청할 서버 주소}
-            HttpMethod.POST, // 요청할 방식
-            kakaoProfileRequest2, // 요청할 때 보낼 데이터
-            String.class // 요청 시 반환되는 데이터 타입
+                "https://kapi.kakao.com/v2/user/me", // https://{요청할 서버 주소}
+                HttpMethod.POST, // 요청할 방식
+                kakaoProfileRequest2, // 요청할 때 보낼 데이터
+                String.class // 요청 시 반환되는 데이터 타입
         );
 
-        System.out.println("response2.getBody(): "+ response2.getBody());
+        System.out.println("response2.getBody(): " + response2.getBody());
 
 
-
-        ObjectMapper objectMapper2= new ObjectMapper();
+        ObjectMapper objectMapper2 = new ObjectMapper();
         KakaoProfile kakaoProfile = objectMapper2.readValue(response2.getBody(),
-            KakaoProfile.class);
-        try{
+                KakaoProfile.class);
+        try {
             kakaoProfile = objectMapper2.readValue(response2.getBody(), KakaoProfile.class);
-        }catch(JsonMappingException e){
+        } catch (JsonMappingException e) {
             e.printStackTrace();
-        }catch (JsonProcessingException e){
+        } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
 
 
         //USER 오브젝트 : userName , password, userEmail
-        System.out.println("카카오 아이디: "+ kakaoProfile.getId());
+        System.out.println("카카오 아이디: " + kakaoProfile.getId());
         System.out.println("카카오 이메일: " + kakaoProfile.getKakao_account().getEmail());
 
-        System.out.println("koun 유저네임: "+ kakaoProfile.getKakao_account().getEmail()+"_"+kakaoProfile.getId());
+        System.out.println("koun 유저네임: " + kakaoProfile.getKakao_account().getEmail() + "_" + kakaoProfile.getId());
         System.out.println("koun 이메일: " + kakaoProfile.getKakao_account().getEmail());
-        System.out.println("userName : "+kakaoProfile.getKakao_account().getProfile().getNickname());
+        System.out.println("userName : " + kakaoProfile.getKakao_account().getProfile().getNickname());
 
         //requestDto 생성
         UserSaveRequestDto kakaoUser = UserSaveRequestDto.builder()
-            .userName(kakaoProfile.getKakao_account().getProfile().getNickname())
-            .userEmail(kakaoProfile.getKakao_account().getEmail())
-            .roleType(RoleType.USER)
-            .isDeleted(false)
-            .oauth("kakao")
-            .build();
+                .userName(kakaoProfile.getKakao_account().getProfile().getNickname())
+                .userEmail(kakaoProfile.getKakao_account().getEmail())
+                .build();
 
 
         // 가입자 혹은 비가입자 체크 해서 처리 -> 로그인 처리
         UserSaveResponseDto originUser = userService.findUser(kakaoUser.getUserEmail());
 
-
         if (originUser.getUserName() == null) {
             Long id = userService.join(kakaoUser);
-            // 새롭게 가입한 사용자의 정보를 세션에 저장
-            sessionManager.createSession(userService.findOne(kakaoUser.getUserEmail()), httpResponse);
-        } else {
-            // 이미 가입한 사용자의 정보를 세션에 저장
-            sessionManager.createSession(originUser,httpResponse );
+            originUser = userService.findUser(kakaoUser.getUserEmail());
         }
 
         System.out.println(originUser.getUserName() + originUser.getUserEmail());
@@ -165,14 +146,12 @@ public class LoginController {
 
 
 
+        HttpHeaders headersJwt = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + token);
+        return new ResponseEntity<>(token, headersJwt, HttpStatus.OK);
 
 
 
-
-
-
-
-        return "loginRedirect";
 
     }
 
